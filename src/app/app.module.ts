f3186a3b8dc2042878723cf99fcc3aea0d03dad1
jygnash <jygnash@gmail.com>
2018-01-16 19:07:16 -0500

    adding the initial layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule, Routes } from '@angular/router';
+import { AlbumsComponent } from './albums/albums.component';
+import { AboutusComponent } from './company/aboutus/aboutus.component';
 
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'albums', component: AlbumsComponent },
+  { path: 'users', component: UsersComponent},
+  { path: 'company/aboutus', component: AboutusComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AlbumsComponent,
+    AboutusComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
